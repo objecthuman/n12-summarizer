da2f6ee383e05dca4b7145b8fe0d2f06e832e9af
Neeraj319 <itskop520@gmail.com>
2025-07-20 14:08:17 +0000

    feat: add pydantic settings

--- src/config.py
+++ src/config.py
@@ -1,5 +1,10 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()  # type: ignore
